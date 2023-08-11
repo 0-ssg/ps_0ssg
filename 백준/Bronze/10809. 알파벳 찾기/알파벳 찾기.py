8edfd6e0a8e7a7c70e3d63a66e658623f70c26b1
0-ssg <128596348+0-ssg@users.noreply.github.com>
2023-08-11 22:34:13 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -7,6 +7,8 @@ a = [-1] * 26
 for i in range(len(S)):
     if a[ord(S[i]) - ord('a')] == -1:
         a[ord(S[i]) - ord('a')] = i
+# a 들어오면 a - a = 0 >> a[0] = 0
+# b 들어오면 b - a = 1 >> a[1] = 1 ...
 
 for i in a:
     print(i, end=' ')
